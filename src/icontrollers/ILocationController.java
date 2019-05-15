012b71d41cf29d92fdec366ad35ff929298c7bcd
okalanang13 <okalanang@gmail.com>
2019-05-15 12:04:36 +0700

    Change ILocationController

--- src/icontrollers/ILocationController.java
+++ src/icontrollers/ILocationController.java
@@ -6,12 +6,13 @@ import java.util.List;
 /**
  *
  * @author RR17
+ * @author Okala
  */
 public interface ILocationController {
     public List<Location> getAll();
-    public Location getById(String locationId);
-    public List<Location> search(Object keyword);
-    public String insert (String locationId, String city, String streetAddress, String postalCode, String stateProvince, String countryId);
-    public String update (String locationId, String city, String streetAddress, String postalCode, String stateProvince, String countryId);
-    public String delete (String locationId);
+    public Location getById(String id);
+    public List<Location> search(String keyword);
+    public String insert(String locationId, String streetAddress, String postalCode, String city, String stateProvince, String countryId);
+    public String update(String locationId, String streetAddress, String postalCode, String city, String stateProvince, String countryId);
+    public String delete(String locationId);
 }
